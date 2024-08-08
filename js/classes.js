f74918fc62edfb4a94d6f5400481a1ca17810f5c
vitoria-csantos <vitoria.santos659@etec.sp.gov.br>
2024-08-08 12:23:03 -0300

    commit

--- js/classes.js
+++ js/classes.js
@@ -1,3 +1,27 @@
-class obj{
-    
+class obj{ //vai construir as imagens do jogo
+
+    constructor(px, py, largura, altura,imagem){
+        this.px = px
+        this.py = py
+        this.largura = largura
+        this.altura = altura
+        this.imagem = imagem
+    }
+
+        draw(){
+            let img = new Image()
+            img.src = this.imagem
+            canvas.drawImage(img, this.px, this.py, this.largura, this.altura)
+        }
+
+}
+
+class BG extends obj{
+    move(velocidade,limite,pos){
+        this.px -= 7 //o número é negativo para dar impressão de que o personagem está se movendo para a direita
+
+        if(this.px < -limite){
+            this.px = pos
+        }
+    }
 }
--- js/game.js
+++ js/game.js
@@ -1,2 +1,23 @@
-const canvas = getElementById("canvas")
-const context = canvas.getContext('2d')
+const canvas = document.getElementById ('canvas').getContext('2d')
+let fundo = new BG(0, 0, 500, 900, 'img/sky.png')
+let fundo2 = new BG(500, 0, 500, 900, 'img/sky.png')
+
+
+function draw(){
+    fundo.draw() //desenha o objeto que a variável recebeu
+    fundo2.draw()
+}
+
+function update(){
+    fundo.move(7,500,0)
+    fundo2.move(7,0,500)
+    
+}
+
+function main(){ 
+canvas.clearRect(0, 0, 500, 900) //limpa o canvas
+update()
+draw()
+}
+
+setInterval(main, 100) //chama a função main a cada 100 milisegundos. Causa a impressão de que está tudo se movendo suavimente
