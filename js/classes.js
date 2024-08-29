883be9f68ced74862d4b2179ec9fa5e649f7d85a
Alex2024Campos <alex.oliveira221@etec.sp.gov.br>
2024-08-29 12:19:44 -0300

    enviando alterações

--- js/classes.js
+++ js/classes.js
@@ -1,4 +1,6 @@
 class obj{ //vai construir as imagens do jogo
+    quadro = 0
+    tempo = 0
 
     constructor(px, py, largura, altura,imagem){
         this.px = px
@@ -14,14 +16,56 @@ class obj{ //vai construir as imagens do jogo
             canvas.drawImage(img, this.px, this.py, this.largura, this.altura)
         }
 
+animacao(velocidade,limite,nomeImg){
+    this.tempo += 1
+    if(this.tempo >= velocidade){
+        this.tempo = 0
+        this.quadro += 1
+    }
+    if(this.quadro >= limite){
+        this.quadro = 0
+    }
+
+    this.imagem = 'img/' + nomeImg + this.quadro + '.png'
+}
 }
 
 class BG extends obj{
     move(velocidade,limite,pos){
-        this.px -= 7 //o número é negativo para dar impressão de que o personagem está se movendo para a direita
+        this.px -= velocidade //o número é negativo para dar impressão de que o personagem está se movendo para a direita
 
         if(this.px < -limite){
             this.px = pos
         }
     }
+}
+
+
+class ground extends BG{
+
+}
+
+
+class BIRD extends obj{
+    velocidade = 2
+    gravidade = 1
+
+    move(){
+       // this.py += this.gravidade
+        if(this.velocidade < 10){
+            this.velocidade += this.gravidade
+        }
+        this.py += this.velocidade
+    }
+
+    limite(){
+        if(this.py >= 660){
+            this.py = 660
+        }
+
+        else if(this.py <= 0){
+            this.py = 0
+        }
+    }
+
 }
--- js/game.js
+++ js/game.js
@@ -1,23 +1,42 @@
 const canvas = document.getElementById ('canvas').getContext('2d')
 let fundo = new BG(0, 0, 500, 900, 'img/sky.png')
 let fundo2 = new BG(500, 0, 500, 900, 'img/sky.png')
+let groundv = new ground(0,700,500,200, 'img/ground.png')
+let ground2 = new ground(500, 700, 500, 200, 'img/ground.png')
 
+let bird = new BIRD(50,400,63,51, 'img/bird0.png')
+
+
+document.addEventListener('click', function(e){
+    bird.velocidade -= 18
+
+})
 
 function draw(){
     fundo.draw() //desenha o objeto que a variável recebeu
     fundo2.draw()
+    groundv.draw()
+    ground2.draw()
+    bird.draw()
 }
 
 function update(){
-    fundo.move(7,500,0)
-    fundo2.move(7,0,500)
-    
+    fundo.move(2,500,0)
+    fundo2.move(2
+        ,0,500)
+    groundv.move(2,500,0)
+    ground2.move(2,0,500)
+
+    bird.move()
+    bird.animacao(8,4,'bird')
+    bird.limite()
 }
 
 function main(){ 
 canvas.clearRect(0, 0, 500, 900) //limpa o canvas
 update()
 draw()
+requestAnimationFrame(main)
 }
 
-setInterval(main, 100) //chama a função main a cada 100 milisegundos. Causa a impressão de que está tudo se movendo suavimente
+main() //chama a função main a cada 100 milisegundos. Causa a impressão de que está tudo se movendo suavimente
